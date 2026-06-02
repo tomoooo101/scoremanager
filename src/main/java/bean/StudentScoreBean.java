739d120d44cd41c7f9ff072be0296a09588d02af
fko2547057 <fko2547057@stu.o-hara.ac.jp>
2026-06-02 14:50:16 +0900

    keit

---
+++ src/main/java/bean/StudentScoreBean.java
@@ -0,0 +1,64 @@
+package bean;
+
+import java.io.Serializable;
+
+public class StudentScoreBean implements Serializable {
+
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+    private String subjectName;
+    private int testNo;
+    private int point;
+
+    public StudentScoreBean() {
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getTestNo() {
+        return testNo;
+    }
+
+    public void setTestNo(int testNo) {
+        this.testNo = testNo;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
